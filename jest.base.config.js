0203b92f79c7d2fafbf31402e075bb2f9bd29144
shani-terminus <shani.brown@terminus.com>
2019-08-01 10:50:12 -0400

    feat(Framework): has basic HTML/TS/SCSS/lint/jest in place
    ISSUES CLOSED: #16

--- jest.base.config.js
+++ jest.base.config.js
@@ -6,7 +6,6 @@ module.exports = {
     },
     '__TRANSFORM_HTML__': true,
   },
-  transform: {'^.+\\.(ts|js|html)$': '<rootDir>/node_modules/jest-preset-angular/preprocessor.js' },
   testMatch: [
     '<rootDir>/**/?(*.)spec.ts?(x)',
     '<rootDir>/**/?(*.)test-sass.js?(x)',
@@ -17,7 +16,6 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!@ngrx)',
   ],
-  reporters: ['default', ['jest-junit', { output: './coverage/junit/report.xml' }]],
   clearMocks: true,
   collectCoverageFrom: [
     'src/**/!(index|public-api|*.module|*.interface|*.constant|*.mock|*.d).ts',
@@ -30,14 +28,9 @@ module.exports = {
     'html',
   ],
   coverageDirectory: '<rootDir>/coverage/',
-  setupFilesAfterEnv: [
-    '<rootDir>/tooling/jest-setup.ts',
-  ],
   moduleNameMapper: {
     'app/(.*)': '<rootDir>/demo/app/$1',
     'assets/(.*)': '<rootDir>/demo/assets/$1',
     'environments/(.*)': '<rootDir>/demo/environments/$1',
-    '^@terminus/ui(.*)$': '<rootDir>/src$1/src/public-api.ts',
-    '^@terminus/ui(.*)/testing$': '<rootDir>/src$1/testing/src/public-api.ts',
   },
 }
